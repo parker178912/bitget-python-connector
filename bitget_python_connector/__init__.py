7ce4e327836abf20712a0aa206c9ff3cd819713a
傳Yee <parker178912@gmail.com>
2023-04-25 23:24:42 +0800

    feat: first commit

---
+++ bitget_python_connector/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.0.5'
+
+from bitget_python_connector.client import *
+from bitget_python_connector.consts import *
+from bitget_python_connector.exceptions import *
+from bitget_python_connector.utils import *
+from bitget_python_connector.broker import *
+from bitget_python_connector.mix import *
+from bitget_python_connector.spot import *
+from bitget_python_connector.ws import *
---
+++ examples/spot_example.py
@@ -0,0 +1,9 @@
+import bitget.spot.public_api as public
+import bitget.spot.market_api as market
+import bitget.spot.account_api as account
+import bitget.spot.order_api as order
+import bitget.spot.plan_api as plan
+
+from bitget.client import Client
+
+Client()
